226da47602c74b68f68500a6e02211d464dfd2d4
Icobart <enrico.cornacchia@studio.unibo.it>
2026-02-23 13:03:21 +0100

    working tests to check door state

--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -1,12 +1,30 @@
 package tdd;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SmartDoorLockTest {
+    private SmartDoorLock smartDoorLock;
+
+    @BeforeEach
+    void beforeEach() {
+        smartDoorLock = new SmartDoorLockImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void testIsNotLocked() {
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testIsNotBlocked() {
+        assertFalse(smartDoorLock.isBlocked());
+    }
+
+    @Test
+    public void testisOpen() {
+        assertFalse(smartDoorLock.isBlocked() && smartDoorLock.isLocked());
     }
 }
